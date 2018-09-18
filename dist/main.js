d45e3c58c85c4115d9c7f53666396bf4c8650d04
Aunik Ahmed <aunik8013@gmail.com>
2018-09-17 22:37:17 -0400

    All commits

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+const parallax1 = document.getElementById("mountain");
+const parallax2 = document.getElementById("ny");
+const parallax3 = document.getElementById("desk");
+const parallax4 = document.getElementById("last");
+
+window.addEventListener("scroll", function() {
+  let offset = window.pageYOffset;
+  parallax1.style.backgroundPositionY = offset * 0.6 + "px";
+  parallax2.style.backgroundPositionY = offset * 0.6 + "px";
+  parallax3.style.backgroundPositionY = offset * 0.5 + "px";
+  parallax4.style.backgroundPositionY = offset * 0.4 + "px";
+});
